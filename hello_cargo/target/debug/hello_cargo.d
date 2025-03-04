eac02947cd335fc6cf59f4a943cb02627506c7d5
Adarsh Jha <adarsh2005j@gmail.com>
2025-03-05 00:07:56 +0530

    rust ga .

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/adarsh/Codes/rust-me/hello_cargo/target/debug/hello_cargo: /home/adarsh/Codes/rust-me/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/adarsh/Codes/rust-me/hello_cargo/target/release/hello_cargo: /home/adarsh/Codes/rust-me/hello_cargo/src/main.rs
